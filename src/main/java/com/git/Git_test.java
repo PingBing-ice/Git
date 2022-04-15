ed17f0e690ff19dfab54299ec5f49293f44c1289
Bing <3521315291@qq.com>
2022-04-15 11:51:23 +0800

    hot-fix commit

--- src/main/java/com/git/Git_test.java
+++ src/main/java/com/git/Git_test.java
@@ -3,5 +3,6 @@ package com.git;
 public class Git_test {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello hot-fix");
     }
 }
