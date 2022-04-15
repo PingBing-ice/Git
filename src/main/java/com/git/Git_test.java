46383b2abe139e6a094ebbca2929f9ea99c6b12c
Bing <3521315291@qq.com>
2022-04-15 11:56:15 +0800

    master commit2

--- src/main/java/com/git/Git_test.java
+++ src/main/java/com/git/Git_test.java
@@ -4,5 +4,6 @@ public class Git_test {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello hot-fix");
+        System.out.println("master Hello hot-fix");
     }
 }
